03754be9471d3da47c4586972e186f467bb177a9
ebinhua <island.huang@ericsson.com>
2019-12-23 22:52:32 +0800

    add script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# replace the string in file
+sed -e 's/true/1/' -e 's/false/0/' file
+
+# more delimiter in awk
+awk -F '[,.]' '{print $5}' file
+awk 'BEGIN{FS="[,@]";OFS=","}{print $0}' file
+
